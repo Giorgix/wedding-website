c6bd81ec63516f859cbdec1576b6805f3d967d8f
Giorgix <jackosinapsis@gmail.com>
2015-05-26 15:43:14 +0200

    redirect to https added

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,16 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+
+router.get('*', function(req, res, next) {
+  console.log(req.secure);
+    if (!req.secure) {
+        //FYI this should work for local development as well
+        return res.redirect('https://' + req.get('host') + req.url);
+    }
+    next();
+});
+
 router.get('/', function(req, res) {
   res.render('index');
 });
