2e503c9a977c8b6276acd95d2421edbd731384ee
Giorgix <jackosinapsis@gmail.com>
2015-06-26 13:02:32 +0200

    added upload resize functionality

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,13 @@ var router = express.Router();
 
 /* GET home page. */
 
-router.get('*', function(req, res, next) {
+/*router.get('*', function(req, res, next) {
     if (!req.secure) {
         //FYI this should work for local development as well
         return res.redirect('https://' + req.connection['servername'] + ':3000' + req.url);
     }
     next();
-});
+});*/
 
 router.get('/', function(req, res) {
   res.render('index');
